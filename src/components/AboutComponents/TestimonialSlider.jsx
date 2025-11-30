2e3f860b2ed8db1529bd75d1c2f1c38a7605e826
Balchandra <balchandra@referloan.in>
2025-11-30 20:52:52 +0530

    chnages

--- src/components/AboutComponents/TestimonialSlider.jsx
+++ src/components/AboutComponents/TestimonialSlider.jsx
@@ -8,30 +8,40 @@ export default function TestimonialSlider() {
     speed: 500,
     autoplay: true,
     autoplaySpeed: 3000,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-        },
+    {
+      breakpoint: 576, 
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
       },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-        },
+    },
+    {
+      breakpoint: 768, 
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
       },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 1,
-        },
+    },
+    {
+      breakpoint: 991, 
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
       },
-    ],
-  };
+    },
+    {
+      breakpoint: 1200, 
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
 
   const testimonials = [
     {
